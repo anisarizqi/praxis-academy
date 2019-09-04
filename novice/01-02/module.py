17f068e10b43141797b53325ce682f6d4c20c4ff
anisarizqi <anisarizqi4@gmail.com>
2019-09-04 13:37:53 +0700

    Dasar Pemograman Python

---
+++ novice/01-02/module.py
@@ -0,0 +1,21 @@
+# Import kelas fungsi
+import funct
+import funct1
+import functmax
+import bubblesort
+import selectsort
+
+# define variabel
+fruits = ["apple", "banana", "cherry"]
+x = 5
+y = 7
+DaftarAngka = [23,7,32,99,4,15,11,20]
+
+#call funtion
+funct.my_function(fruits)
+funct1.say_hello()
+functmax.print_max(x, y)
+bubblesort.BubbleSort(DaftarAngka)
+print(DaftarAngka)
+selectsort.SelectionSort(DaftarAngka)
+print(DaftarAngka)
